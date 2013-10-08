ec654b1a11012a7820807cd29fe65a6427f300d4
Axel Wagner <mail@merovius.de>
2013-10-08 16:56:49 +0200

    Use pointer instead of index

--- golf.c
+++ golf.c
@@ -1,25 +1,25 @@
 #define c(a,x) case a: x; break;
-#define o(a,r) c(a, s[--i] = s[i] r s[i+1])
-s[2000],i=-1;
+#define o(a,r) c(a, *--s = *s r *(s+1))
+t[2000];
 main(d) {
-    char q[2001],*p=q;
+    char q[2001],*p=q,*s=t;
     gets(p);
     for (;;) {
         switch (*p) {
-            c(46, printf("%d\n", s[i--]))
+            c(46, printf("%d\n", *s--))
             c(118, d = 80)
             c(94, d = -80)
             c(62, d = 1)
             c(60, d = -1)
-            c(124, d = s[i--] ? -80 : 80)
-            c(95, d = s[i--] ? -1 : 1)
+            c(124, d = *s-- ? -80 : 80)
+            c(95, d = *s-- ? -1 : 1)
             o(43, +)
             o(45, -)
             o(47, /)
             o(42, *)
             c(32, )
             default:
-                s[++i] = *p - '0';
+                *++s = *p - '0';
                 break;
             c(64, return 0)
         }
