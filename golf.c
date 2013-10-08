fed1a817b88072dc5d27d8ae4dc772da8518ee5d
Axel Wagner <mail@merovius.de>
2013-10-08 16:51:25 +0200

    #define cases

--- golf.c
+++ golf.c
@@ -1,49 +1,26 @@
+#define c(a,x) case a: x; break;
 s[2000],i=-1;
 main(d) {
     char q[2001],*p=q;
     gets(p);
     for (;;) {
         switch (*p) {
-            case 46:
-                printf("%d\n", s[i--]);
-                break;
-            case 118:
-                d = 80;
-                break;
-            case 94:
-                d = -80;
-                break;
-            case 62:
-                d = 1;
-                break;
-            case 60:
-                d = -1;
-                break;
-            case 124:
-                d = s[i--] ? -80 : 80;
-                break;
-            case 95:
-                d = s[i--] ? -1 : 1;
-                break;
-            case 43:
-                s[--i] = s[i] + s[i+1];
-                break;
-            case 45:
-                s[--i] = s[i] - s[i+1];
-                break;
-            case 47:
-                s[--i] = s[i] / s[i+1];
-                break;
-            case 42:
-                s[--i] = s[i] * s[i+1];
-                break;
-            case 32:
-                break;
+            c(46, printf("%d\n", s[i--]))
+            c(118, d = 80)
+            c(94, d = -80)
+            c(62, d = 1)
+            c(60, d = -1)
+            c(124, d = s[i--] ? -80 : 80)
+            c(95, d = s[i--] ? -1 : 1)
+            c(43, s[--i] = s[i] + s[i+1])
+            c(45, s[--i] = s[i] - s[i+1])
+            c(47, s[--i] = s[i] / s[i+1])
+            c(42, s[--i] = s[i] * s[i+1])
+            c(32, )
             default:
                 s[++i] = *p - '0';
                 break;
-            case 64:
-                return 0;
+            c(64, return 0)
         }
         p += d;
     }
