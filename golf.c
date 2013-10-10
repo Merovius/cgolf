70da40d21ca8ff3a58e5d2a3a890ff0f44d2ee0c
Axel Wagner <mail@merovius.de>
2013-10-10 22:05:43 +0200

    Remove whitespace

--- golf.c
+++ golf.c
@@ -1,21 +1,3 @@
-#define c(a,x) *p+3==*#a?x:
-#define o(a,r) c(a, (*++s = *s r *(s-1), d))
-main(d) {
-    char q[2001],*p=q,*s;
-    for (gets(p);;)
-        p+=d=
-        c(1, (printf("%d\n", *s++),d))
-        c(y, 80)
-        c(a, -80)
-        c(A, 1)
-        c(?, -1)
-        c(, *s++ ? -80 : 80)
-        c(b, *s++ ? -1 : 1)
-        o(., +)
-        o(0, -)
-        o(2, /)
-        o(-, *)
-        c(#, d)
-        c(C, p)
-        (*--s = *p - 48, d);
-}
+#define c(a,x)*p+3==*#a?x:
+#define o(a,r)c(a,(*++s=*s r*(s-1),d))
+main(d){char q[2001],*p=q,*s;for(gets(p);;)p+=d=c(1,(printf("%d\n",*s++),d))c(y,80)c(a,-80)c(A,1)c(?,-1)c(,*s++?-80:80)c(b,*s++?-1:1)o(.,+)o(0,-)o(2,/)o(-,*)c(#,d)c(C,p)(*--s=*p-48,d);}
